274d7fc509ef90abcbd91816d99f1875bec8f8d2
afschoolmadhisland25 <afschoolmadhisland25@gmail.com>
2025-12-31 16:42:18 +0530

    add: changes

---
+++ folders.js
@@ -0,0 +1,35 @@
+const foldersData = {
+  annualDayCelebrations: {
+    directory: "Images/Annual Day Celebrations/",
+    images: [
+      "img1.jpeg",
+      "img2.jpeg",
+      "img3.jpeg",
+      "img4.jpeg",
+      "img5.jpeg",
+      "img6.jpeg",
+      "img7.jpeg",
+    ],
+    description: "Memorable moments from our Annual Day celebrations",
+    name: "Annual Day Celebrations",
+  },
+  independenceDayCelebrations: {
+    directory: "Images/Independence Day Celebrations/",
+    images: ["img1.jpeg", "img2.jpeg"],
+    description: "Celebrating our nation's independence with pride and joy",
+    name: "Independence Day Celebrations",
+  },
+  parentsReviews: {
+    directory: "Images/Parents Reviews/",
+    images: ["vid1.mp4", "vid2.mp4", "vid3.mp4", "vid4.mp4", "vid5.mp4"],
+    description: "Reviews and testimonials from our parents",
+    name: "Parents Reviews",
+  },
+
+  achievements: {
+    directory: "Images/Achievements/",
+    images: ["img1.jpeg", "img2.jpeg", "img3.jpeg", "img4.jpeg"],
+    description: "Our achievements and accomplishments",
+    name: "Achievements",
+  },
+};
---
+++ notices.js
@@ -0,0 +1,47 @@
+const noticeBoardData = [
+  {
+    day: "10",
+    month: "Each Quarter",
+    title: "Fee Payment Deadlines",
+    description:
+      "Fee payment is due on the 10th of the starting month of every quarter.",
+    tag: "Important",
+  },
+  {
+    day: "28",
+    month: "Nov",
+    title: "Annual Day Celebration",
+    description:
+      "The Annual Day will be celebrated on November 28. All students and parents are invited.",
+    tag: "Events",
+  },
+  {
+    day: "Jan",
+    month: "",
+    title: "Annual Sports Meet",
+    description:
+      "The Annual Sports Meet will be held in January. All students are requested to participate actively.",
+    tag: "Sports",
+  },
+  {
+    day: "08",
+    month: "May",
+    title: "Summer Vacation",
+    description: "Summer vacation will be from 08 May 2025 to 20 Jun 2025.",
+    tag: "Holiday",
+  },
+  {
+    day: "27",
+    month: "Sep",
+    title: "Autumn Break",
+    description: "Autumn break will be from 27 Sep 2025 to 06 Oct 2025.",
+    tag: "Holiday",
+  },
+  {
+    day: "23",
+    month: "Dec",
+    title: "Winter Break",
+    description: "Winter break will be from 23 Dec 2025 to 01 Jan 2026.",
+    tag: "Holiday",
+  },
+];
