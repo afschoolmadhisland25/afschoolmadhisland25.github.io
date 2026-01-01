3f2e9935bdc952533d68a62dfdb5f5c7bc8fbdc3
afschoolmadhisland25 <afschoolmadhisland25@gmail.com>
2026-01-01 13:22:25 +0530

    add: changes

--- folders.js
+++ folders.js
@@ -33,3 +33,171 @@ const foldersData = {
     name: "Achievements",
   },
 };
+
+const photosData = [
+  {
+    src: "gallery/Annual function.jpeg",
+    title: "Annual function",
+    category: "events",
+  },
+  {
+    src: "gallery/Annual health checkup.jpeg",
+    title: "Annual health checkup",
+    category: "events",
+  },
+  {
+    src: "gallery/Baisakhi celebration.jpeg",
+    title: "Baisakhi celebration",
+    category: "events",
+  },
+  {
+    src: "gallery/Best out of waste competition.jpeg",
+    title: "Best out of waste competition",
+    category: "academic",
+  },
+  {
+    src: "gallery/Christmas celebration.jpeg",
+    title: "Christmas celebration",
+    category: "events",
+  },
+  {
+    src: "gallery/Circletime.jpeg",
+    title: "Circletime",
+    category: "activities",
+  },
+  {
+    src: "gallery/Cotton pasting activity.jpeg",
+    title: "Cotton pasting activity",
+    category: "activities",
+  },
+  {
+    src: "gallery/Diwali Celebration.jpeg",
+    title: "Diwali Celebration",
+    category: "events",
+  },
+  {
+    src: "gallery/Face mask making activity.jpeg",
+    title: "Face mask making activity",
+    category: "activities",
+  },
+  {
+    src: "gallery/First day at school.jpeg",
+    title: "First day at school",
+    category: "events",
+  },
+  {
+    src: "gallery/Fruits Day celebration.jpeg",
+    title: "Fruits Day celebration",
+    category: "events",
+  },
+  {
+    src: "gallery/Ganesh Chatruthi.jpeg",
+    title: "Ganesh Chatruthi",
+    category: "events",
+  },
+  {
+    src: "gallery/Green day celebration.jpeg",
+    title: "Green day celebration",
+    category: "events",
+  },
+  {
+    src: "gallery/Hybrid mode of learning.jpeg",
+    title: "Hybrid mode of learning",
+    category: "academic",
+  },
+  {
+    src: "gallery/Independance Day.jpeg",
+    title: "Independance Day",
+    category: "events",
+  },
+  {
+    src: "gallery/Janamashtami celebration.jpeg",
+    title: "Janamashtami celebration",
+    category: "events",
+  },
+  {
+    src: "gallery/MTS Staff.jpeg",
+    title: "MTS Staff",
+    category: "campus",
+  },
+  {
+    src: "gallery/National space day.jpeg",
+    title: "National space day",
+    category: "events",
+  },
+  {
+    src: "gallery/Shivangi pandey Teacher in Charge.jpeg",
+    title: "Shivangi pandey Teacher in Charge",
+    category: "campus",
+  },
+  {
+    src: "gallery/sports.jpeg",
+    title: "Sports",
+    category: "sports",
+  },
+  {
+    src: "gallery/Swachh mahotsav.jpeg",
+    title: "Swachh mahotsav",
+    category: "events",
+  },
+  {
+    src: "gallery/Teachers.jpeg",
+    title: "Teachers",
+    category: "campus",
+  },
+  {
+    src: "gallery/Tree plantation.jpeg",
+    title: "Tree plantation",
+    category: "events",
+  },
+  {
+    src: "gallery/WhatsApp Image 2025-11-02 at 15.09.28.jpeg",
+    title: "School Event",
+    category: "campus",
+  },
+  {
+    src: "building/building 23.23.15.jpeg",
+    title: "School Building",
+    category: "building",
+  },
+  {
+    src: "building/building 23.23.43.jpeg",
+    title: "School Building",
+    category: "building",
+  },
+  {
+    src: "building/building 23.24.29.jpeg",
+    title: "School Building",
+    category: "building",
+  },
+  {
+    src: "building/building 23.24.53.jpeg",
+    title: "School Building",
+    category: "building",
+  },
+  {
+    src: "building/building 23.25.17.jpeg",
+    title: "School Building",
+    category: "building",
+  },
+  {
+    src: "building/building 23.25.26.jpeg",
+    title: "School Building",
+    category: "building",
+  },
+  {
+    src: "building/building 23.25.36.jpeg",
+    title: "School Building",
+    category: "building",
+  },
+  {
+    src: "building/seated.jpeg",
+    title: "School Building",
+    category: "building",
+  },
+  {
+    src: "building/seats.jpeg",
+    title: "School Building",
+    category: "building",
+  },
+];
